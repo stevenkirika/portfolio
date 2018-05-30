d180386f04dbb3d04b0505d678e840bada92f0b3
Steven Kirika <stevenkirika12@gmail.com>
2018-05-30 11:26:16 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Resume | Steven Kirika </title>
+  <link rel="shortcut icon" href="img/favicon.png" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container site-main">
+	<header>
+		<div class="row">
+			<div class="col-md-12">
+				 <h1 class="h1_title_name">Steven Kirika</h1> 
+                 <p class="p_title_name">Web / Software  Designer &amp; Developer</p>
+			</div>
+		</div>
+	</header>
+	<div class="top-divider"></div>
+	<section id="aboutme">
+		<div class="row">
+			<div class="col-md-5">
+				<div class="data-list">
+			<h1>AVAILABLE FOR :</h1>
+			<h3 style="margin-top: -15px;">HIRE AND FREELANCE WORK</h3>
+				 <h2><i class="icon-user"></i>  Name</h2>
+				 <ul><li>Steven Kirika Ndegwa</li></ul>
+				 <h2><i class="icon-phone"></i>  Phone</h2>
+				 <ul><li>+254 786 922328</li></ul>
+				 
+				 <h2><i class="icon-email"></i> Email Address</h2>
+				 <ul><li>stevenkirika12@gmail.com</li></ul>
+                
+			</div>
+			</div>
+			<div class="col-md-7">
+				 <h1 class="personal_h1">Personal Summary</h1>
+			 	<p align="justify">
+				I am a competent and committed Web and Software Designer and Developer looking for a rewarding career in the Information Technology sector. I am highly focused with a comprehensive knowledge of designing and developing the layout, visual appearance and usability of a website and applications. I possess a proven track record of successfully completing projects from the concept through to design, testing and handover. </p>
+                <p align="justify">
+				I am easy going and able to get along with both work colleagues and managers. I am currently looking for a suitable web and software developer position with a progressive employer.
+				</p>
+
+			</div>
+		</div>
+		
+	</section>
+	<div class="top-divider"></div>
+	<section style="padding: 5px 0;">
+		<div class="row">
+			<div class="col-md-6">
+				 <h1 class="personal_h1"><i class="icon2-education"></i>Education</h1>
+      			 <div class="data-list">
+			     <h2><i class="icon-cert"></i> Present &raquo; Msc : Computer Systems</h2>
+				 <ul><li>Jomo Kenyatta University Of Agriculture &amp; Technology</li></ul>
+				 <h2><i class="icon-cert"></i> 2010 - 2013 &raquo; Bsc : Computer Science</h2>
+				 <ul><li>Catholic University Of Eastern Africa</li></ul>
+				 <h2><i class="icon-cert"></i> 2008 - 2010 &raquo; Diploma : Business Information &amp; Technology</h2>
+				 <ul><li>Jomo Kenyatta University Of Agriculture &amp; Technology</li></ul>
+				 <h2><i class="icon-cert"></i> 2010 &raquo; Oracle Database10g : Administration</h2>
+				 <ul><li>New Horizon College</li></ul>
+				</div>
+
+			</div>
+			<div class="col-md-6">
+				<h1 class="personal_h1"><i class="icon2-skills"></i>Personal Skills</h1>
+                <div class="data-list">
+				 <ul>
+                <li><i class="icon-skills"></i> Professional experience of coding tools and technologies including PHP, MySQL, HTML5, CSS3, jQuery, VB.NET, Java &amp; C#.</li>
+                <li><i class="icon-skills"></i> Extensive understanding of Wordpress CMS &amp; Oscommerce e-commerce platform;  Substantial knowledge of Drupal &amp; Joomla CMS </li>
+                <li><i class="icon-skills"></i> Technical Skills: Netbeans, VB.NET, PHP/PHP Framework(LARAVEL), MySQL, Adobe design products and Dreamweaver. </li> 
+                <li><i class="icon-skills"></i> Have a track record of high-quality web application interfaces and design implementations.</li> 
+                <li><i class="icon-skills"></i> Adaptable and able to quickly pick up new techniques. </li>
+                 </ul>
+			    </div>
+			</div>
+
+		</div>
+	</section>
+	<div class="top-divider"></div>
+	<section style="padding: 5px 0;">
+		<div class="row">
+				<div class="col-md-12">
+		        <h1 class="personal_h1"><i class="icon2-watch"></i>Work Samples</h1>
+		       </div>
+		</div>
+		<div class="row masonry-container">
+
+				<div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.panini.co.ke"  data-toggle="tooltip"  title="Ecommerce - Panini Restaurant Restaurant - www.panini.co.ke" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/panini.jpg" alt="Panini Restaurant Restaurant">
+		          </a>
+		        </div>
+
+		        <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.andy.or.ke" data-toggle="tooltip"  title="Wordpress CMS - Action Network For the Disabled - www.andy.or.ke" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/andy.jpg" alt="Action Network For the Disabled">
+		          </a>
+		        </div>
+		        <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.blazeconsulting.co.ke" data-toggle="tooltip"  title="Wordpress CMS - Blaze Consulting - www.blazeconsulting.co.ke" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/Blazeconsulting.jpg" alt="Blaze Consulting">
+		          </a>
+		        </div>
+
+		        <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.farmbizafrica.com" data-toggle="tooltip"  title="Joomla CMS - Farmbiz Africa - www.farmbizafrica.com" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/farmbizafrica.jpg" alt="Farmbiz Africa">
+		          </a>
+		        </div>
+		         <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.lit.co.ke" data-toggle="tooltip"  title="Ecommerce - Living Images Technologies - www.lit.co.ke" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/lit.jpg" alt="Living Images Technologies">
+		          </a>
+		        </div>
+
+		        <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://beyondglamourafrica.com/" data-toggle="tooltip"  title="Wordpress CMS - Beyond Glamour Africa - www.beyondglamourafrica.com" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/bga.jpg" alt="Beyond Glamour Africa">
+		          </a>
+		        </div>
+
+		          <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://beyondglamourafrica.com/" data-toggle="tooltip"  title="Wordpress CMS - Joseph Kyule Photographer - www.josephkyulephotography.com" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/josephkyule.jpg" alt="Joseph Kyule Photographer">
+		          </a>
+		        </div>
+
+		          <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="https://girlsinkenyakillingit.com/" data-toggle="tooltip"  title="Wordpress CMS - Girls In Kenya Killing It - www.girlsinkenyakillingit.com" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/girls_in_kenya_killing.jpg" alt="Girls In Kenya Killing It">
+		          </a>
+		        </div>
+
+		         <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="https://raadvocates.co.ke/" data-toggle="tooltip"  title="Wordpress CMS - Rachael Advocates - www.raadvocates.co.ke" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/raadvocates.jpg" alt="Rachael Advocates">
+		          </a>
+		        </div>
+
+		         <div class="col-lg-3 col-md-3 col-xs-6">
+		          <a class="d-block mb-4 h-100" href="http://www.jabelieducationcentre.com/" data-toggle="tooltip"  title="Wordpress CMS - Jabeli Education Center &amp; Adult Education - www.jabelieducationcentre.com" target="_blank">
+		            <img class="img-fluid img-thumbnail" src="img/work/jabeli_education_center_&_adult_education.jpg" alt="Jabeli Education Center &amp; Adult Education">
+		          </a>
+		        </div>
+			
+		</div>
+	</section>
+    <div class="top-divider"></div>
+
+</div>
+<script type="text/javascript" src="js/jquery.masonry.min.js"></script>
+<script type="text/javascript">
+	$(function () {
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $('.masonry-container').masonry({itemSelector: '.col-md-3'});
+})
+	
+</script>
+</body>
+</html>
